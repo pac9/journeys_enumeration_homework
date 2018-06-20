892ced88d5a4299218e8e04acbe405bd3842abfc
pac9 <pac9@btinternet.com>
2018-06-20 17:52:52 +0100

    minor amendments to formatting

--- models/traveller.js
+++ models/traveller.js
@@ -50,13 +50,4 @@ Traveller.prototype.getUniqueModesOfTransport = function () {
   return uniqueItems;
 }
 
-
-// function onlyUnique(value, index, self) {
-//     return self.indexOf(value) === index;
-// }
-//
-// // usage example:
-// var a = ['a', 1, 'a', 2, '1'];
-// var unique = a.filter( onlyUnique ); // returns ['a', 1, 2, '1']
-
 module.exports = Traveller;
